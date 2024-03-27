4489ba43851c092c8617f028c9663ab6a44cbdae
Olena Pobehailo <106855523+OlenaPobehailo@users.noreply.github.com>
2024-03-27 14:32:07 +0200

    get list of teachers from firebase

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
@@ -7,6 +8,7 @@ const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
 const STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET;
 const MESSAGING_SENDER_ID = import.meta.env.VITE_MESSAGING_SENDER_ID;
 const APP_ID = import.meta.env.VITE_APP_ID;
+const DATA_BASE_URL = import.meta.env.VITE_DATA_BASE_URL;
 
 const firebaseConfig = {
   apiKey: API_KEY,
@@ -15,8 +17,11 @@ const firebaseConfig = {
   storageBucket: STORAGE_BUCKET,
   messagingSenderId: MESSAGING_SENDER_ID,
   appId: APP_ID,
+  databaseURL: DATA_BASE_URL,
 };
 
 export const app = initializeApp(firebaseConfig);
 
 export const auth = new getAuth();
+
+export const database = getDatabase(app);
--- src/pages/TeachersPage.jsx
+++ src/pages/TeachersPage.jsx
@@ -1,11 +1,47 @@
-import { StyledCommonWrapper } from "../styles/CommonStyled"
+import { get, getDatabase, ref } from "firebase/database";
+import { StyledCommonWrapper } from "../styles/CommonStyled";
+import { useEffect, useState } from "react";
 
 const TeachersPage = () => {
+  const [teachers, setTeachers] = useState(null);
+
+  useEffect(() => {
+    const db = getDatabase();
+    const teachersRef = ref(db, "teachers");
+    console.log(db);
+    console.log(teachersRef);
+
+    get(teachersRef)
+      .then((snapshot) => {
+        console.log(snapshot);
+
+        if (snapshot.exists()) {
+          setTeachers(Object.values(snapshot.val()));
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error("Error getting data:", error);
+      });
+  }, []);
+
   return (
     <StyledCommonWrapper>
-      TeachersPage
+      <h1>Teachers:</h1>
+      {teachers ? (
+        teachers.map((teacher, index) => (
+          <div key={index}>
+            <p>
+              {teacher.name} {teacher.surname}
+            </p>
+          </div>
+        ))
+      ) : (
+        <p>No data available</p>
+      )}
     </StyledCommonWrapper>
-  )
-}
+  );
+};
 
-export default TeachersPage
+export default TeachersPage;
